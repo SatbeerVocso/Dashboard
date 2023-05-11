269f259a4d1d4514b9cdcc0ba5487939f0986e41
SatbeerVocso <131966513+SatbeerVocso@users.noreply.github.com>
2023-05-11 16:23:28 +0530

    changes in processes component 1

--- src/pages/Processes/Processes.js
+++ src/pages/Processes/Processes.js
@@ -1,19 +1,31 @@
-import React, { useState } from "react"
-import { Card, CardBody, input, Button, Input, Label, Form } from "reactstrap"
+import React, { useState, useEffect } from "react"
+import { Card, CardBody, Button, Form, UncontrolledAlert } from "reactstrap"
 import ProcessesForm from "./ProcessesForm"
 import { motion } from "framer-motion"
 import TextInput from "common/TextInput"
 
 function Processes() {
   const [request, setrequest] = useState("")
-
+  const [showAlert, setShowAlert] = useState(false)
   const [submit, setsubmit] = useState(false)
 
-  const submithandler = (e) => {
+  const submithandler = e => {
     e.preventDefault()
+    if (request === "") {
+      setShowAlert(true)
+      return
+    }
     setsubmit(true)
   }
-  
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowAlert(false)
+    }, 2000) // hide the alert message after 5 seconds
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [showAlert])
 
   return (
     <div className="page-content">
@@ -29,13 +41,20 @@ function Processes() {
       ) : (
         <Card style={{ width: "50%", margin: "auto" }} className="mt-4">
           <CardBody>
+            {showAlert && (
+              <UncontrolledAlert color="danger" className="mb-3" role="alert">
+                Please enter the request name
+              </UncontrolledAlert>
+            )}
             <Form onSubmit={submithandler}>
               <div>
-                <TextInput 
-                label='Enter the Request Name'
-                type='text' 
-                value={request}
-                onChange={(e)=>{setrequest(e.target.value)}}
+                <TextInput
+                  label="Enter the Request Name"
+                  type="text"
+                  value={request}
+                  onChange={e => {
+                    setrequest(e.target.value)
+                  }}
                 />
               </div>
               <Button color="success" className="mt-4" type="submit">
--- src/pages/Processes/ProcessesField.js
+++ src/pages/Processes/ProcessesField.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Label, Input, UncontrolledAlert } from "reactstrap"
 import AllFieldType from "./AllFieldType"
 import Processfieldata from "./Processfieldata"
@@ -8,10 +8,11 @@ import TextInput from "common/TextInput"
 function ProcessesField(props) {
   const [FieldName, setFieldName] = useState("")
   const [FieldType, setFieldType] = useState("")
-  
+
   const [isInput2Clicked, setisInput2Clicked] = useState(false)
   const [FieldSubmit, setFieldSubmit] = useState(false)
   const [isVisible, setisVisible] = useState(true)
+  const [showAlert, setShowAlert] = useState(false)
 
   const handleFieldTypeSelected = fieldType => {
     setFieldType(fieldType)
@@ -19,7 +20,10 @@ function ProcessesField(props) {
     //this function is created for to get data from allfieldtype component  and setthe value into setfiedtype
   }
   const fieldSubmithandler = () => {
-    // console.log(FieldName, FieldType)
+    if (FieldName == "" || FieldType == "") {
+      setShowAlert(true)
+      return
+    }
     setFieldSubmit(true)
     //this function is called on saving the data and onclick of icons save this function is called
   }
@@ -27,7 +31,15 @@ function ProcessesField(props) {
     setisVisible(false)
     //initially component is visible but when trash icon is clicked we hide the component
   }
- 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShowAlert(false)
+    }, 2000) // hide the alert message after 5 seconds
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [showAlert])
   const containerStyle = {
     display: "flex",
     width: "100%",
@@ -55,14 +67,24 @@ function ProcessesField(props) {
           ) : (
             <div style={containerStyle}>
               <div style={halfWidthStyle} className="mt-2 ">
-                <TextInput 
-                  label='Enter the Field Name'
-                  type='text'
+                {showAlert && (
+                  <UncontrolledAlert
+                    color="danger"
+                    className="mb-3"
+                    role="alert"
+                  >
+                    Please enter the field name & field type !
+                  </UncontrolledAlert>
+                )}
+
+                <TextInput
+                  label="Enter the Field Name"
+                  type="text"
                   value={FieldName}
                   onChange={e => {
                     setFieldName(e.target.value)
                   }}
-                  />
+                />
               </div>
               <div style={halfWidthStyle} className="mt-2">
                 <Label className="mb-0">Enter the Field Type</Label>
